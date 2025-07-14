19142c4573197d0dde795fdf8b7be854a93fdd5e
Abdullahkhan871 <akhan283785@gmail.com>
2025-07-14 13:25:49 +0530

    refactor: Clean up authRouter.js by removing commented code and organizing imports

--- _02_ecommerce/routes/authRouter.js
+++ _02_ecommerce/routes/authRouter.js
@@ -1,3 +1,10 @@
-// signup
-// login
-// logout
+import e from "express";
+import { login, logout, signup } from "../controllers/user.controller.js";
+const authRouter = e.Router();
+
+
+authRouter.post("/signup", signup);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
+
+export default authRouter;
